569b3215fea6a88ab559396eeddadc9283a00a0a
Polykrovka <2595092@gmail.com>
2021-02-21 18:45:24 +0300

    fix: remome console.log in hanoi-towel.js

--- src/hanoi-tower.js
+++ src/hanoi-tower.js
@@ -1,9 +1,7 @@
 
 
 module.exports = function calculateHanoi(disksNumber, turnsSpeed) {
-  console.log(disksNumber, turnsSpeed);
   let turns = 2 ** disksNumber - 1;
   let seconds = Math.floor(turns * 3600 / turnsSpeed);
-  console.log(turns, seconds)
   return {'turns': turns, 'seconds': seconds};
 };
