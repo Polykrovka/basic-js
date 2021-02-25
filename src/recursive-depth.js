bca4330912d441c9270562753d2df49b11f008cd
Polykrovka <2595092@gmail.com>
2021-02-25 21:46:13 +0300

    fix: remove console.log in recursive-depth.js

--- src/recursive-depth.js
+++ src/recursive-depth.js
@@ -3,7 +3,6 @@
 module.exports = class DepthCalculator {
   
   calculateDepth( arr ) {
-    console.log(arr);
     if(Array.isArray(arr)){
       return 1 + Math.max(0, ...arr.map(item => this.calculateDepth(item)))
     } else {
