0cee1c371de6cec5164d5491577510f7b41d9ccf
Polykrovka <2595092@gmail.com>
2021-03-01 19:29:32 +0300

    fix: change the default for additionSeparator

--- src/extended-repeater.js
+++ src/extended-repeater.js
@@ -6,13 +6,13 @@ module.exports = function repeater(str, options) {
   let string = String(str);
   let repeatTimes = (options.repeatTimes || 1);
   let separator = (options.separator || '+');
-  let additionSeparator = (options.additionSeparator || '')
+  let additionSeparator = (options.additionSeparator || '|')
   let addition = (typeof options.addition === 'boolean' || options.addition === null) ? String(options.addition) : (options.addition || '');
   let additionRepeatTimes = (options.additionRepeatTimes || 1);
   if(additionRepeatTimes !== 1){
     addition = (addition + additionSeparator).repeat(additionRepeatTimes).slice(0, - additionSeparator.length);
   }
-
+  
   anser += ((separator + string + addition).repeat(repeatTimes).substring(separator.length));
   return anser;
 };
