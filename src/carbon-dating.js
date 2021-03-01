3599eab74f556c5947b313c2fd3ca3d893d69836
Polykrovka <2595092@gmail.com>
2021-03-01 19:15:22 +0300

    fix: update task "carbon-dating"

--- src/carbon-dating.js
+++ src/carbon-dating.js
@@ -3,20 +3,21 @@ const MODERN_ACTIVITY= 15;
 const HALF_LIFE_PERIOD= 5730;
 
 module.exports = function dateSample(sampleActivity) {
+
 if(sampleActivity == null || typeof(sampleActivity) !== "string" || sampleActivity === Infinity || isNaN(sampleActivity) ){
     return false;
 }
-if(sampleActivity.length < 15){
+if(+sampleActivity > 15 || +sampleActivity <= 0){
     return false;
 }
 let k = 0.693 / HALF_LIFE_PERIOD;
-let t = Math.ceil(MODERN_ACTIVITY / sampleActivity) / k;
-console.log(t);
+let t = Math.ceil(Math.log(MODERN_ACTIVITY / sampleActivity) / k);
 if(t === Infinity) {
     return false;
 }
+
 return t;
 
-// Формула t = ln(Nнул / N) / (0.693 / HALF_LIFE_PERIOD)
 
+// Формула t = ln(Nнул / N) / (0.693 / HALF_LIFE_PERIOD)
 };
