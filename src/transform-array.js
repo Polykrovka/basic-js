f439b92972b2443a2abb73fbfac12652c03cd887
Polykrovka <2595092@gmail.com>
2021-03-04 22:11:20 +0300

    feat: Add "transform-array" task

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,45 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if(!Array.isArray(arr)){
+    throw Error("It isn't an array")
+  }
+
+  let ansver = [];
+  let exist = false;
+  
+  for(let i = 0; i < arr.length; i++){
+    switch (arr[i]) {
+      case '--discard-next':
+        i += 1;
+        exist = false;
+        break;
+
+      case '--discard-prev':
+      if(exist){
+        ansver.pop()
+      }
+        break;
+
+
+      case '--double-next':
+        if(i < arr.length - 1){  
+          ansver.push(arr[i + 1]) 
+        }
+        break;
+
+      case '--double-prev':
+        if(exist){
+          ansver.push(arr[i - 1])
+        }
+        break;
+
+      default:
+        ansver.push(arr[i]);
+        exist = true;
+    }
+  }
+  return ansver;
+
 };
+
