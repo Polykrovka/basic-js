fd93a09f3727b8cb55b35ee7da3a55214212ce4f
Polykrovka <2595092@gmail.com>
2021-03-25 12:19:45 +0300

    Add solytion VigenereCipher task

--- src/vigenere-cipher.js
+++ src/vigenere-cipher.js
@@ -1,14 +1,80 @@
 const CustomError = require("../extensions/custom-error");
 
 class VigenereCipheringMachine {
-  encrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
-  }    
-  decrypt() {
-    throw new CustomError('Not implemented');
-    // remove line with error and write your code here
+  constructor(trueOrFalse) {
+    this.trueOrFalse = trueOrFalse;
   }
+  
+
+  encrypt(str, key) {
+    if (typeof str !== 'string' || typeof key !== 'string') {
+      throw new Error;
+    }
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    let strArr = str.toUpperCase().split('');
+    let keyArr = key.toUpperCase().split('');
+    let j = 0;
+    let symbolCode = 0;
+    let result = [];
+    for(let i = 0; i < str.length; i++){
+      if (keyArr[j] === undefined) {
+        j = 0;             // ключ кончился, начни сначала
+      }
+      if (!alphabet.includes(strArr[i])) {
+        result.push(strArr[i]); 
+        continue;
+        
+      }
+      symbolCode = strArr[i].charCodeAt() + keyArr[j].charCodeAt() - 65;
+      if (symbolCode > 90) {
+        symbolCode -= 26;   //букв в алфавите на случай если индекс больше Z
+      }
+      result.push(String.fromCharCode(symbolCode))
+      j += 1;
+    }
+
+    if (this.trueOrFalse === false) {
+      return result.reverse().join('');
+    } else {
+      return result.join('');
+    } 
+
+  }    
+  decrypt(str, key) {
+    if (typeof str !== 'string' || typeof key !== 'string') {
+      throw new Error;
+    }
+    let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".split('');
+    let strArr = str.toUpperCase().split('');
+    let keyArr = key.toUpperCase().split('');
+    let j = 0;
+    let symbolCode = 0;
+    let result = [];
+    for(let i = 0; i < str.length; i++){
+      if (keyArr[j] === undefined) {
+        j = 0;             // ключ кончился, начни сначала
+      }
+      if (!alphabet.includes(strArr[i])) {
+        result.push(strArr[i]);  
+        continue;        // Для посторонних символов ключ не нужен
+      }
+      symbolCode = strArr[i].charCodeAt() - keyArr[j].charCodeAt() + 65;
+      if (symbolCode < 65) {
+        symbolCode += 26;   //букв в алфавите на случай если индекс меньше A
+      }
+      result.push(String.fromCharCode(symbolCode))
+      j += 1;
+    }
+    if (this.trueOrFalse === false) {
+      return result.reverse().join('');
+    } else {
+      return result.join('');
+    } 
+      
+    
+  }    
 }
+
+
 
 module.exports = VigenereCipheringMachine;
